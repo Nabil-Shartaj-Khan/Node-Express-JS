5a5988783433ec96eaf138c746992ee7f3bf8eb1
Nabil-Shartaj-Khan <shartajnabil@gmail.com>
2023-12-03 12:56:58 +0600

    added package.json

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-// server creation
+// server creation, url module to parse URL
 
 var http=require('http');
+var url=require('url');
 
 
 var server = http.createServer(function(req, res)
 {   
-    res.end("Hello Nabil");
+
+    // request response model in detail, defining response
+
+    if (req.url=='/'){
+
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.write('<h1>Welcome to the Home page</h1>')
+         res.end();
+
+    }
+    else if (req.url=='/contact'){
+
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write('<h1>Welcome to the Contact page</h1>')
+        res.end();
+    }
+    
+    else if (req.url=='/about'){
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write('<h1>Welcome to the About page</h1>')
+        res.end();
+    }
 
 });
 
